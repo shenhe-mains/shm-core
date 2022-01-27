9d94aa145faf4f156cbfda39bb5e910deb9b0789
hyper-neutrino <alex.yj.liao@gmail.com>
2022-01-26 19:27:52 -0500

    choose command

--- src/modules/fun/main.js
+++ src/modules/fun/main.js
@@ -1,9 +1,26 @@
 const { ButtonInteraction } = require("discord.js");
+const { ArgumentError, Success } = require("../../errors");
 const { checkCount } = require("../../utils");
 
-exports.commands = {};
+exports.commands = { choose: choose };
 
 exports.listeners = { interactionCreate: [checkFun] };
+
+async function choose(ctx, args, body) {
+    var options;
+    if (body.match(",")) {
+        options = body.split(",").map((s) => s.trim());
+    } else {
+        options = args;
+    }
+    if (options.length == 0) {
+        throw new ArgumentError("Please provide at least one choice.");
+    }
+    throw new Success(
+        "Choice",
+        options[Math.floor(Math.random() * options.length)]
+    );
+}
 
 async function checkFun(client, interaction) {
     if (!(interaction instanceof ButtonInteraction)) return;
