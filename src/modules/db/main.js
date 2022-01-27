e82327df8063cdeb2169032392e415b6f0af83a5
hyper-neutrino <alex.yj.liao@gmail.com>
2022-01-26 23:23:00 -0500

    add db command

---
+++ src/modules/db/main.js
@@ -0,0 +1,44 @@
+const { has_permission } = require("../../core/privileges");
+const { PermissionError, Success } = require("../../errors");
+const { client } = require("../../db");
+const { pagify } = require("../../pages");
+
+exports.commands = {
+    db: dbquery,
+};
+
+async function dbquery(ctx, args, body) {
+    if (!has_permission(ctx.author, "database")) {
+        throw new PermissionError(
+            "You do not have permission to query the database."
+        );
+    }
+    const result = await client.query(body);
+    if (result.rows.length == 0) {
+        throw new Success(
+            "Database Query Result",
+            "Your database query did not return any rows."
+        );
+    }
+    const fields = [];
+    const field_names = rows.fields.map((field) => field.name);
+    for (const entry of result.rows) {
+        fields.push(
+            "```" +
+                field_names
+                    .map((name) => name + ": " + entry[name])
+                    .join("\n") +
+                "```"
+        );
+    }
+    await pagify(
+        ctx,
+        {
+            title: "Database Query Result",
+            color: "GREEN",
+        },
+        fields,
+        5
+    );
+    throw new Success();
+}
