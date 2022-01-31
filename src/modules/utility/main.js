8c13969fcbadc624f5b9c79b605619b1eb3d54ab
hyper-neutrino <alex.yj.liao@gmail.com>
2022-01-31 16:54:30 -0500

    add point command

--- src/modules/utility/main.js
+++ src/modules/utility/main.js
@@ -1,12 +1,14 @@
-const { Info, PermissionError } = require("../../errors");
+const { Info, PermissionError, UserError } = require("../../errors");
 const { checkCount, censor_attachments } = require("../../utils");
 const { ranks, has_permission } = require("../../core/privileges");
-const { reload } = require("../../core/config");
+const { reload, modify, config } = require("../../core/config");
 
 exports.commands = {
     ranks: _ranks,
     "reload-config": reload_config,
     clone: clone,
+    webhook: webhook,
+    point: point,
 };
 
 async function _ranks(ctx, args) {
@@ -36,9 +38,9 @@ async function reload_config(ctx, args) {
 
 async function clone(ctx, args) {
     checkCount(args, 2);
-    if (!has_permission(ctx.author, "settings")) {
+    if (!has_permission(ctx.author, "send")) {
         throw new PermissionError(
-            "You do not have permission to command the bot like that."
+            "You do not have permission to command the bot to send message."
         );
     }
     const channel = await ctx.parse_channel(args[0]);
@@ -50,4 +52,40 @@ async function clone(ctx, args) {
     };
     if (message.content) response.content = message.content;
     await channel.send(response);
+}
+
+async function webhook(ctx, args) {
+    checkCount(args, 1);
+    if (!has_permission(ctx.author, "webhook")) {
+        throw new PermissionError(
+            "You do not have permission to alter or get the webhook."
+        );
+    }
+    modify({
+        webhook: args[0],
+    });
+}
+
+async function point(ctx, args) {
+    checkCount(args, 1);
+    if (!has_permission(ctx.author, "webhook")) {
+        throw new PermissionError(
+            "You do not have permission to alter or get the webhook."
+        );
+    }
+    const channel = await ctx.parse_channel(args[0]);
+    const webhooks = await ctx.guild.fetchWebhooks();
+    const webhook = webhooks.get(config.webhook);
+    if (webhook === undefined) {
+        throw new UserError(
+            `This server's webhook is not set or the configured webhook no longer exists. ` +
+                `Please set it using \`${config.prefix}webhook <id>\` (note: copy the ID and not the URL, as exposing the URL is a security issue).`
+        );
+    }
+    await webhook.edit({
+        channel: channel,
+    });
+    try {
+        await ctx.author.send(webhook.url);
+    } catch {}
 }
