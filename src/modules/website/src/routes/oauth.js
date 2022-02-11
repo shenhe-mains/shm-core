7986f7352b4bc6bccb2910d42d254b60def4baa0
hyper-neutrino <alex.yj.liao@gmail.com>
2022-02-11 18:25:00 -0500

    bruh

--- src/modules/website/src/routes/oauth.js
+++ src/modules/website/src/routes/oauth.js
@@ -2,6 +2,7 @@ const { app } = require("../app");
 const data = require("../../../../../data");
 const { config } = require("../../../../core/config");
 const { default: fetch } = require("node-fetch");
+const { flash } = require("../utils");
 
 app.param("destination", function (req, res, next, destination) {
     req.destination = destination;
@@ -24,7 +25,11 @@ app.get("/discord-oauth/:destination", function (req, res) {
         .then((response) => response.json())
         .then((json) => {
             if (json.access_token === undefined) {
-                res.redirect("/oauth-failed/");
+                flash(
+                    req,
+                    "Something unexpected went wrong during the OAuth process. Please try again."
+                );
+                res.redirect("/");
             } else {
                 req.session.discord_token = json.access_token;
                 res.redirect("/" + req.destination + "/");
