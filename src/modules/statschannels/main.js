bcb8f15eb3e84709596b1eb16a43fd8febe37504
hyper-neutrino <alex.yj.liao@gmail.com>
2022-02-11 00:27:55 -0500

    fml

--- src/modules/statschannels/main.js
+++ src/modules/statschannels/main.js
@@ -7,11 +7,16 @@ exports.listeners = {
 };
 
 async function check_boosters(client, before, after) {
-    if (before.guild.id != config.guild) return;
-    const channel = await client.channels.fetch(config.channels.boost_count);
-    await channel.edit({
-        name: `✧・ Boosts: ${before.guild.premiumSubscriptionCount}`,
-    });
+    setTimeout(() => {
+        if (before.guild.id != config.guild) return;
+        await client.channels
+            .fetch(config.channels.boost_count)
+            .then((channel) =>
+                channel.edit({
+                    name: `✧・ Boosts: ${before.guild.premiumSubscriptionCount}`,
+                })
+            );
+    }, 10000);
 }
 
 async function check_member_count(client, member) {
