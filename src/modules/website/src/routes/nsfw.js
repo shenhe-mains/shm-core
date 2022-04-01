4262d6d655f9a7682a8f893720ed0393cce08ec7
hyper-neutrino <alex.yj.liao@gmail.com>
2022-03-31 20:35:19 -0400

    add nsfw jebait counter

--- src/modules/website/src/routes/nsfw.js
+++ src/modules/website/src/routes/nsfw.js
@@ -1,7 +1,10 @@
 const { app } = require("../app");
 const { render } = require("../utils");
 
+let clicks = 0;
+
 app.get("/nsfw/", (req, res) => {
+    console.log(`${++clicks} horny disciple(s) got jebaited!`);
     res.send(
         render(req, "nsfw/index.pug", {
             title: "NSFW Access",
